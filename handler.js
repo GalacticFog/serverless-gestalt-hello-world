6985f390f6c236886c39e2fb7f98886144b3394c
John Betancur <jbetancur@users.noreply.github.com>
2018-08-10 08:33:27 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -9,8 +9,5 @@ module.exports.hello = (event, context, callback) => {
     }),
   };
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  callback(null, JSON.stringify(response));
 };
